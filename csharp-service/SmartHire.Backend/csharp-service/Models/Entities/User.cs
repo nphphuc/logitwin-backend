a0ac5dc97f803ae27962ece63d9b336a3a5b7e6d
phatom2005 <khoinguyen.vo910@gmail.com>
2026-03-01 17:40:24 +0700

    dbcontext-connect

---
+++ csharp-service/SmartHire.Backend/csharp-service/Models/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace csharp_service.Models.Entities;
+
+public partial class User
+{
+    public Guid Id { get; set; }
+
+    public string CognitoSub { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string? FullName { get; set; }
+
+    public string? Role { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+}
